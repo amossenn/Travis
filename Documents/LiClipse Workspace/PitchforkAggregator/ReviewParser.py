960528ebb46baeac4ed498b60a8d052ed5648516
amossenn <austinmatthewme@gmail.com>
2017-08-24 15:07:53 -0700

    Artist and Album is the only info with unique class titles

---
+++ Documents/LiClipse Workspace/PitchforkAggregator/ReviewParser.py
@@ -0,0 +1,17 @@
+#ReviewParser.py
+#Scripted by: Austin Moss-Ennis
+#Description: Grabs a pitchfork review and parses the Artist, Album Name,
+# Release Date, Genre, Review Score, Review Author, and Review Date
+def reviewParser():
+    import requests
+    from bs4 import BeautifulSoup
+    #Grabs html information from the Pitchfork website
+    req = requests.get('http://pitchfork.com/reviews/albums/22835-homogenic/')
+    #Parses the information to make it more easily traversable
+    clean = BeautifulSoup(req.text, "html.parser")
+    album = clean.h1
+    artist = clean.h2
+    print album
+    print artist
+    
+reviewParser()
